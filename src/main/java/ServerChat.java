5987f8d64ed64aa641f0cbe7d074419e2fbb7e0c
Pedro Sarmento <77494920+Pedro-Sarmento@users.noreply.github.com>
2024-05-29 21:15:25 +0100

    2

---
+++ src/main/java/ServerChat.java
@@ -0,0 +1,96 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.lang.management.OperatingSystemMXBean;
+import java.util.concurrent.atomic.AtomicInteger;
+import Zookeeper.ZookeeperClient;
+
+@SpringBootApplication
+@EnableWebSocketMessageBroker
+public class ServerChat implements WebSocketMessageBrokerConfigurer {
+
+    private ZookeeperClient zooKeeperClient;
+    private AtomicInteger currentLoad = new AtomicInteger(0);
+    private String serverNodePath;
+    private final SimpMessagingTemplate messagingTemplate;
+
+    @Autowired
+    public ServerChat(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerChat.class, args);
+    }
+
+    @Bean
+    public ZookeeperClient zooKeeperClient() throws IOException {
+        return new ZookeeperClient();
+    }
+
+    @PostConstruct
+    public void init() {
+        try {
+            this.zooKeeperClient = new ZookeeperClient();
+            serverNodePath = "/servers/server-" + System.currentTimeMillis();
+            zooKeeperClient.registerNewServer(serverNodePath, String.valueOf(currentLoad.get()));
+            measureSystemLoad();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/chat-socket").setAllowedOrigins("*").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @PostMapping("/send")
+    public void sendMessage(@RequestParam String from, @RequestParam String to, @RequestParam String content) {
+        String destination = "/topic/messages/" + to;
+        Message message = new Message(from, to, content);
+        updateLoad(true);
+        messagingTemplate.convertAndSend(destination, message);
+    }
+
+    //AQUI
+    @PostMapping("/send-login")
+    public void sendMessage(@RequestParam String username, @RequestParam String password) {
+        String destination = "/topic/messages/" + to;
+        Message message = new Message(from, to, content);
+        updateLoad(true);
+        messagingTemplate.convertAndSend(destination, message);
+    }
+
+    public void updateLoad(boolean increase) {
+        int newLoad = increase ? currentLoad.incrementAndGet() : currentLoad.decrementAndGet();
+        try {
+            zooKeeperClient.updateServerLoad(serverNodePath, String.valueOf(newLoad).getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void measureSystemLoad() {
+        OperatingSystemMXBean osBean = ManagementFactory.getOperatingSystemMXBean();
+        double systemLoad = osBean.getSystemLoadAverage();
+        int adjustedLoad = Math.min(100, (int) (systemLoad * 100.0));
+        updateLoad(adjustedLoad);
+    }
+}
--- src/main/java/Zookeeper/ZookeeperClient.java
+++ src/main/java/Zookeeper/ZookeeperClient.java
@@ -1,8 +1,12 @@
+package Zookeeper;
+
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
-import com.mongodb.client.model.Projections;
 import org.apache.zookeeper.*;
 import org.apache.zookeeper.data.Stat;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -19,7 +23,8 @@ public class ZookeeperClient {
     CountDownLatch connectionLatch = new CountDownLatch(1);
     private static final String host = "localhost:2181";
     private Database database = new Database();
-    
+    private final RestTemplate restTemplate = new RestTemplate();
+
     public ZooKeeper connect() throws IOException, InterruptedException {
          zoo = new ZooKeeper(host, 3000, new Watcher(){
              public void process(WatchedEvent we){
@@ -47,12 +52,13 @@ public class ZookeeperClient {
         zoo.close();
     }
 
-    public void registerNewServer(String path, byte[] data) throws KeeperException, InterruptedException{
+    public void registerNewServer(String path,String ip, int load) throws KeeperException, InterruptedException{
+        String data = ip + ":" + load;
         Stat stat = zoo.exists(path, false);
         if(stat == null){
-            zoo.create(path, data, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+            zoo.create(path, data.getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
         } else{
-            zoo.setData(path, data, stat.getVersion());
+            zoo.setData(path, data.getBytes(), stat.getVersion());
         }
     }
 
@@ -60,15 +66,19 @@ public class ZookeeperClient {
         List<String> servers= zoo.getChildren("/servers", false);
         String bestServer = null;
         int minLoad = Integer.MAX_VALUE;
+        String bestServerIp = null;
         for(String server : servers){
             byte[] serverData = zoo.getData("/servers/ +server", false, null);
+            String[] dataParts = new String(serverData).split(":");
+            String ip = dataParts[0];
             int load = Integer.parseInt(new String(serverData));
             if(load < minLoad){
                 minLoad = load;
                 bestServer = server;
+                bestServerIp = ip;
             }
         }
-        return bestServer;
+        return bestServerIp;
     }
 
     public void createEPH(String path, String name) throws KeeperException, InterruptedException{
@@ -93,4 +103,26 @@ public class ZookeeperClient {
         return messageList;
     }
 
+    @PostMapping("/send-login")
+    public String sendLogin(@RequestBody String username, @RequestBody String password) {
+        try {
+            String bestServer = selectBestServer();
+            String serverUrl = "http://" + bestServer + ":8080/send-login";
+            restTemplate.postForObject(serverUrl, null, String.class, username, password);
+        } catch (Exception e) {
+            return "Error: " + e.getMessage();
+        }
+        return "";
+
+    }
+    public String sendMessage(@RequestParam String from, @RequestParam String to, @RequestParam String content) {
+        try {
+            String bestServer = selectBestServer();
+            String serverUrl = "http://" + bestServer + ":8080/send-message";
+            restTemplate.postForObject(serverUrl, null, String.class, from, to, content);
+            return "Message sent to " + to + " via server " + bestServer;
+        } catch (Exception e) {
+            return "Error: " + e.getMessage();
+        }
+    }
 }
